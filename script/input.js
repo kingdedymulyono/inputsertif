e792002aabd815e66c419c4da587ad52fdd990f4
kingdedymulyono <githubdedyas@gmail.com>
2025-09-19 23:18:18 +0700

    add input pages v1

---
+++ script/input.js
@@ -0,0 +1,25 @@
+
+const test = document.getElementById('test')
+const formName = document.getElementById("formName")
+
+if (localStorage.getItem("murid")) {
+    fetch("../data/murid.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((murid) => {
+                if (murid.nisn == localStorage.getItem("murid")) {
+                    formName.innerHTML=`Hello ${murid.nama}`
+                }
+            })
+        })
+    } else {
+        fetch("../data/murid.json")
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((murid) => {
+                if (murid.nisn == 202523192) {
+                    formName.innerHTML=`Hello , ${murid.nama}`
+                }
+            })
+        })
+}
--- script/script.js
+++ script/script.js
@@ -1,48 +1,53 @@
-const submitBtn=document.getElementById("submitBtn")
-const nis=document.getElementById("nis")
-let students = []
+const submitBtn = document.getElementById("submitBtn")
+const nis = document.getElementById("nis")
+let students
 
 const success = (name) => {
     swal.fire({
-        icon:'success',
-        title:`Selamat datang ${name}`,
-        text:'Lanjutkan ke halaman berikutnya',
-        confirmButtonColor:'#A5DC86',
-        confirmButtonText:'Lanjutkan'
+        icon: 'success',
+        title: `Selamat datang ${name}`,
+        text: 'Lanjutkan ke halaman berikutnya',
+        confirmButtonColor: '#A5DC86',
+        confirmButtonText: 'Lanjutkan'
+    }).then((result)=>{
+        if(result.isConfirmed){
+            location.href="../pages/input.html"
+        }
     })
 }
 
 const error = () => {
     swal.fire({
-        icon:'error',
-        title:`Oops`,
-        text:'Something Went Wrong',
-        confirmButtonColor:'#F27474',
-        confirmButtonText:'Try Again'
+        icon: 'error',
+        title: `Oops`,
+        text: 'Something Went Wrong',
+        confirmButtonColor: '#F27474',
+        confirmButtonText: 'Try Again'
     })
 }
 
 const getData = async (id) => {
-    let isFound=false
+    let isFound = false
     fetch("../data/murid.json")
-    .then(response=>response.json())
-    .then(data=>{
-        data.forEach(murid => {
-            if(murid.nisn==id){
-                isFound=true
-                success(murid.nama)
-            }    
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(murid => {
+                if (murid.nisn == id) {
+                    isFound = true
+                    localStorage.setItem("murid", murid.nisn)
+                    success(murid.nama)
+                }
+            })
+            if (!isFound) {
+                console.log('error  ')
+            }
         })
-        if(!isFound){
-            console.log('error  ')
-        }
-    })
 }
-submitBtn.addEventListener("click",async ()=>{
-    if(nis.value){
+submitBtn.addEventListener("click", async () => {
+    if (nis.value) {
         await getData(nis.value)
-        nis.value=''
-    }else{
+        nis.value = ''
+    } else {
         error()
     }
 })
